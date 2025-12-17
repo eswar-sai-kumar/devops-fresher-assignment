0b2dcc5ad19089262f1f42aabdec414d1870790e
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-12-17 11:42:32 +0530

    corrected bracket mistakes

--- docker/terraform/main.tf
+++ docker/terraform/main.tf
@@ -11,12 +11,12 @@ module "docker" {
   vpc_security_group_ids = ["sg-043783df234d1a42d"] 
   subnet_id = "subnet-0c5143ffdb39af59d" 
   ami = data.aws_ami.ami_info.id
-  root_block_device = [
+  root_block_device = {
    {
     volume_size = 50        
     volume_type = "gp3"      
    }
-  ]
+  }
 
   tags = {
     Name = "docker"
