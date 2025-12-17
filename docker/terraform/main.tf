47a1ab92948aba17779401e02ef2a3f67cdc1db6
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-12-17 12:08:29 +0530

    created secutity group for the instance

--- docker/terraform/main.tf
+++ docker/terraform/main.tf
@@ -2,6 +2,31 @@ resource "aws_key_pair" "docker-key-pair" {
   key_name   = "docker-key-pair"
   public_key = file("c:/repos/key-pair.pub")
 }
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh_access"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allows access from any IP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # Allows all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh-access-sg"
+  }
+}
+
 module "docker" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   key_name = aws_key_pair.docker-key-pair.key_name
@@ -12,10 +37,8 @@ module "docker" {
   subnet_id = "subnet-0c5143ffdb39af59d" 
   ami = data.aws_ami.ami_info.id
   root_block_device = {
-   {
     volume_size = 50        
     volume_type = "gp3"      
-   }
   }
 
   tags = {
