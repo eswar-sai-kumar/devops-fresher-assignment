bbfa6cf1974c674254c5c97041e254e3b33dd6ca
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-12-17 12:13:42 +0530

    added sg to instance module

--- docker/terraform/main.tf
+++ docker/terraform/main.tf
@@ -33,7 +33,7 @@ module "docker" {
   name = "docker"
 
   instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-043783df234d1a42d"] 
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   subnet_id = "subnet-0c5143ffdb39af59d" 
   ami = data.aws_ami.ami_info.id
   root_block_device = {
